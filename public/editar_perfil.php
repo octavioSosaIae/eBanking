9412f63d06bd7ebbdbcaab1da0c95cc78a1f6cd6
emanueloverlay <emanueloverlay@outlook.com>
2024-09-02 15:14:32 -0300

    Update password change

--- public/editar_perfil.php
+++ public/editar_perfil.php
@@ -35,20 +35,20 @@ require_once __DIR__ . "/assets/template/header.php";
 <br>
 <section id="restablecer-contrasena">
     <h2>Restablecer Contraseña</h2>
-    <form class="form-style">
+    <form class="form-style" id="changePassword">
         <div class="form-group">
-            <label for="contrasena-actual">Contraseña Actual</label>
-            <input type="password" id="contrasena-actual" name="contrasena-actual" placeholder="Contraseña Actual" required>
+            <label for="current_password">Contraseña Actual</label>
+            <input type="password" id="current_password" name="current_password" placeholder="Contraseña Actual" required>
         </div>
 
         <div class="form-group">
-            <label for="nueva-contrasena">Nueva Contraseña</label>
-            <input type="password" id="nueva-contrasena" name="nueva-contrasena" placeholder="Nueva Contraseña" required>
+            <label for="new_password">Nueva Contraseña</label>
+            <input type="password" id="new_password" name="new_password" placeholder="Nueva Contraseña" required>
         </div>
 
         <div class="form-group">
-            <label for="confirmar-contrasena">Confirmar Nueva Contraseña</label>
-            <input type="password" id="confirmar-contrasena" name="confirmar-contrasena" placeholder="Confirmar Nueva Contraseña" required>
+            <label for="confirm_password">Confirmar Nueva Contraseña</label>
+            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirmar Nueva Contraseña" required>
         </div>
 
         <div class="form-group">
@@ -57,7 +57,7 @@ require_once __DIR__ . "/assets/template/header.php";
     </form>
 </section>
 
-
+<script src="assets/js/profile.js"></script>
 <?php
     require_once __DIR__ . "/assets/template/footer.php";
 ?>
